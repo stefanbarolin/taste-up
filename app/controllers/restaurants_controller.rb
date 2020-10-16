7701d7c3747536c6e790e3a9879a7c234fa3b464
alexis <alexis@hellomybusiness.fr>
2020-10-16 16:33:21 +0200

    ok

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,6 @@
 class RestaurantsController < ApplicationController
   before_action :init_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
     @restaurants = Restaurant.all
   end
@@ -13,9 +14,12 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-    raise
-    redirect_to restaurants_path
+    @restaurant.user = current_user
+    if @restaurant.save
+     redirect_to restaurants_path
+    else
+      new
+    end
   end
 
   def edit
@@ -26,7 +30,6 @@ class RestaurantsController < ApplicationController
     @restaurant.save
     redirect_to restaurants_path(@restaurant)
   end
-
   def destroy
     @restaurant.destroy
     redirect_to restaurants_path
@@ -39,6 +42,6 @@ class RestaurantsController < ApplicationController
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :adress, :category, :user_id, :picture_url)
+    params.require(:restaurant).permit(:name, :adress, :category, :picture_url)
   end
 end
