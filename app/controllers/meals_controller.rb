d6bd78aeac319cfeb3361a915dcf4104d737b8c1
alexis <alexis@hellomybusiness.fr>
2020-10-17 12:15:03 +0200

    créa de menu à la mano

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -48,6 +48,6 @@ class MealsController < ApplicationController
   private
 
   def meal_params
-    params.require(:meal).permit(:restaurant_id, :picture_url, :price, :description, :name, :category)
+    params.require(:meal).permit(:restaurant_id, :picture_url, :price, :description, :name)
   end
 end
