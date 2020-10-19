6925dc8acfbe3cd11bc5714672556ec5c9a6e40b
Stefan BAROLIN <stefan.barolin@gmail.com>
2020-10-20 01:14:35 +0200

    Commit before remove

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,1 +1,11 @@
-AlgoliaSearch.configuration = { application_id: APPLICATION_ID, api_key: ALGOLIA_API_KEY }
+AlgoliaSearch.configuration = { application_id: 'APPLICATION_ID', api_key: 'ALGOLIA_API_KEY' }
+
+AlgoliaSearch.configuration = {
+  application_id: 'APPLICATION_ID',
+  api_key: 'ALGOLIA_API_KEY',
+  connect_timeout: 2,
+  receive_timeout: 30,
+  send_timeout: 30,
+  batch_timeout: 120,
+  search_timeout: 5
+}
