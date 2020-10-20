d1a497fc6e46f92ac7a20d2d74274c284646c4ec
Stefan BAROLIN <stefan.barolin@gmail.com>
2020-10-20 17:53:40 +0200

    Adding algolia search

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,3 +1,11 @@
 class Meal < ApplicationRecord
+  include AlgoliaSearch
+
+  algoliasearch do
+    attributes :name, :description, :picture_url
+
+    searchableAttributes ['name', 'description', 'unordered(picture_url)']
+  end
+
   belongs_to :restaurant
 end
