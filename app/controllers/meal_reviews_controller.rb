9cda98b03a7ffaa30e0435f615c23d7bb425680f
afchine said <afzongho.code@gmail.com>
2020-10-24 11:10:29 +0200

    mealReviews added with post working

--- app/controllers/meal_reviews_controller.rb
+++ app/controllers/meal_reviews_controller.rb
@@ -8,6 +8,7 @@ class MealReviewsController < ApplicationController
   def create
     @meal_review = MealReview.new(reviews_params)
     @meal_review.meal = Meal.find(params[:meal_id])
+    @meal_review.user = current_user
       if @meal_review.save
         redirect_to meal_path(@meal_review.meal)
       else
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,14 +26,16 @@ require("channels")
 import "bootstrap";
 
 // Internal imports, e.g:
+import { initStarRating } from '../plugins/init_star_rating';
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initStarRating();
 });
 
 //  import the function ratings for meal reviews
-import { initStarRating } from '../plugins/init_star_rating';
 
-initStarRating();
+
+
